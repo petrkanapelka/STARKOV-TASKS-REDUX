ba23ce9bef719f0ac1a3fc3d137ab90e3cbe4740
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-23 15:14:02 +0300

    refactor: DeckItem

--- src/features/decks/DecksList/DeckItem/DeckItem.tsx
+++ src/features/decks/DecksList/DeckItem/DeckItem.tsx
@@ -1,7 +1,8 @@
+import { ItemType } from '../../decks-api'
 import s from './DeckItem.module.css'
 
 type DeckProps = {
-  deck: any // todo: fix
+  deck: ItemType // todo: fix
 }
 
 const TEST_ACC_NAME = 'kukus'
