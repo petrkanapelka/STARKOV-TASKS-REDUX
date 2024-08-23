68c423adc8f12cc800fc10afd7778a5357764176
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-23 15:12:56 +0300

    feat: update DecksList

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,16 @@
+import { useEffect } from 'react'
 import s from './DecksList.module.css'
+import { decksApi } from '../decks-api'
 
 export const DecksList = () => {
+  useEffect(() => {
+    decksApi.getDecks()
+      .then(res => {
+        console.log("🚀 ~ useEffect ~ res ➔", res);
+      })
+      .catch((error)=>{
+        console.error(error);
+      })
+  })
   return <ul className={s.list}></ul>
 }
