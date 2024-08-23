01d39e46686f8d0d7e5ab17c14628cab03bbed39
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-23 15:12:42 +0300

    feat: update decks-api

--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -6,3 +6,39 @@ export const instance = axios.create({
     'x-auth-skip': true,
   },
 })
+
+export const decksApi = {
+  getDecks() {
+    return instance.get<ResponseType>('/v2/decks')
+  },
+}
+
+export type ResponseType = {
+  items: ItemType[]
+  pagination: PaginationType
+}
+
+export type AuthorType = {
+  id: string
+  name: string
+}
+
+export type PaginationType = {
+  currentPage: number
+  itemsPerPage: number
+  totalPages: number
+  totalItems: number
+}
+
+export type ItemType = {
+  isFavorite: true
+  author: AuthorType
+  id: string
+  userId: string
+  name: string
+  isPrivate: true
+  cover: string
+  created: string
+  updated: string
+  cardsCount: number
+}
