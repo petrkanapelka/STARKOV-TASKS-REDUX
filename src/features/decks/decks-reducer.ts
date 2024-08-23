fc7b67c9824a522029da5e6f401b6bf46d7756d3
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-23 15:13:37 +0300

    feat: refactor deckReducer

--- src/features/decks/decks-reducer.ts
+++ src/features/decks/decks-reducer.ts
@@ -1,5 +1,7 @@
+import { ItemType } from './decks-api'
+
 const initialState = {
-  decks: [] as any[], // todo: add type
+  decks: [] as ItemType[], // todo: add type
   searchParams: {
     name: '',
   },
@@ -9,10 +11,16 @@ type DecksState = typeof initialState
 
 export const decksReducer = (state: DecksState = initialState, action: DecksActions): DecksState => {
   switch (action.type) {
+    case 'SET-DECKS': {
+      return { ...state, decks: action.decks }
+    }
     default:
       return state
   }
 }
 
-type DecksActions = any
+type DecksActions = ReturnType<typeof setDecksAC>
 
+export const setDecksAC = (decks: ItemType[]) => {
+  return { type: 'SET-DECKS', decks } as const
+}
