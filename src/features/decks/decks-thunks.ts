67a3ea493e19ef40ccc8a1496a2a2aade627ec98
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-25 15:16:25 +0300

    feat: update decks-thunks

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -1,9 +1,15 @@
 import { Dispatch } from 'redux'
 import { decksApi } from './decks-api'
-import { setDecksAC } from './decks-reducer'
+import { addDeckAC, setDecksAC } from './decks-reducer'
 
 export const fetchDecksTC = (dispatch: Dispatch) => {
   decksApi.getDecks().then((res) => {
     dispatch(setDecksAC(res.data.items))
+  })
+}
+
+export const addDeckTC = (name: string) => (dispatch: Dispatch) => {
+  decksApi.createDeck(name).then((res) => {
+    dispatch(addDeckAC(res.data))
   })
 }
