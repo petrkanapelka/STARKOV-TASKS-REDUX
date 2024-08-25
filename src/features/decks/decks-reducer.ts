f4f26daeb13aac6b18dc80a470694b46a9082f9f
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-25 15:16:06 +0300

    feat: update decks-reducer

--- src/features/decks/decks-reducer.ts
+++ src/features/decks/decks-reducer.ts
@@ -15,14 +15,22 @@ export const decksReducer = (state: DecksState = initialState, action: DecksActi
     case 'SET-DECKS': {
       return { ...state, decks: action.decks }
     }
+
+    case 'ADD-DECKS': {
+      const newDeck = action.deck
+      return { ...state, decks: [newDeck, ...state.decks] }
+    }
     default:
       return state
   }
 }
 
-type DecksActions = ReturnType<typeof setDecksAC>
+type DecksActions = ReturnType<typeof setDecksAC> | ReturnType<typeof addDeckAC>
 
 export const setDecksAC = (decks: ItemType[]) => {
   return { type: 'SET-DECKS', decks } as const
 }
 
+export const addDeckAC = (deck: ItemType) => {
+  return { type: 'ADD-DECKS', deck } as const
+}
